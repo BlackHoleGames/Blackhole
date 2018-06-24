ebca48eb13956c739e49cb67ef8cc1934e51f449
Franky Pear <FrankPear85@hotmail.com>
2018-06-24 21:03:25 +0200

    Adding ScrollParallax

---
+++ Assets/Scripts/Background/BackGroundScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScroller : MonoBehaviour {
+
+    public float scrollSpeed;//inferior a 1 para mover suave
+
+    private Vector3 startPosition; //scala de y
+    private float tileSize;//scala de y
+
+    // Use this for initialization
+    void Start () {
+        startPosition = transform.position;
+        tileSize = transform.localScale.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Movimiento para la nueva posición de z
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSize);//entre 0 y tilesize
+        transform.position = startPosition + Vector3.forward * newPosition; //x = 0 , y =0 y z nueva posición
+
+    }
+}
---
+++ Assets/TimerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerScript : MonoBehaviour {
+
+    public void TimeLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchron(sceneIndex));
+        //AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+//        operation.progress
+    }
+    IEnumerator LoadAsynchron(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
