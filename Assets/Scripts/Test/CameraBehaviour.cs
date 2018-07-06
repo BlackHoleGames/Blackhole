042d6331c3f90e264d566f9b502377c2aa44572a
Max72590 <max-725@hotmail.com>
2018-07-06 21:56:59 +0200

    made a switchable from vertical to horizontal shooter, tried smooth transition with the camera but needs fixing in one direction

---
+++ Assets/Scripts/Test/CameraBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+
+    public Vector3 verticalPosition;
+    public Vector3 horizontalPosition;
+    public Vector3 verticalRotation;
+    public Vector3 horizontalRotation;
+    public float speed = 5.0f;
+    public bool startRotating;
+    public bool toVerticalPos;
+	// Use this for initialization
+	void Start () {
+        startRotating = false;
+        toVerticalPos = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (startRotating) {
+            /*float rotY = transform.rotation.eulerAngles.z;
+            if (!toVerticalPos)
+            {
+                rotY -=  Time.deltaTime * speed;
+                if (rotY > -90.0f)
+                {
+                    rotY = -90.0f;
+                    startRotating = false;
+                }
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f,rotY);
+            }
+            else {
+                rotY += Time.deltaTime * speed;
+                if (rotY < 0.0f)
+                {
+                    rotY = 0.0f;
+                    startRotating = false;
+                }
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f,rotY);
+            }*/
+        }
+	}
+
+    public void switchCamPosRot(bool verticalOrientation) {
+         Quaternion q = Quaternion.identity;
+         if (verticalOrientation) {
+             transform.position = verticalPosition;
+             transform.rotation = Quaternion.Euler(verticalRotation.x, verticalRotation.y, verticalRotation.z) ;
+         }
+         else
+         {
+             transform.position = horizontalPosition;
+             transform.rotation = Quaternion.Euler(horizontalRotation.x, horizontalRotation.y, horizontalRotation.z);
+         }
+        //startRotating = true;
+        //toVerticalPos = verticalOrientation;
+    }
+
+
+}
---
+++ Assets/Scripts/Test/SwitchablePlayerController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchablePlayerController : MonoBehaviour {
+
+
+    public bool is_vertical;
+    public float fireCooldown;
+    public float speedFactor = 1.0f;
+    public GameObject projectile;
+    public float XLimit = 10.0f;
+    public float ZLimit = 5.0f;
+    public float invul = 1.0f;
+    public float sloMo = 2.0f;
+    private float firingCounter;
+    private bool is_firing, spaceDown, accelDown, firing ;
+    private int ghostCounter;
+    private TimeManager tm;
+    public CameraBehaviour cb;
+    public GameObject sphere;
+    public GameObject ghost;
+    public AudioSource timebomb;
+    public AudioSource slomo;
+    // Use this for initialization
+    void Start()
+    {
+        firingCounter = 0.0f;
+        ghostCounter = 0;
+        is_firing = false;
+        spaceDown = false;
+        accelDown = false;
+        is_vertical = true;
+        tm = GetComponent<TimeManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axisX = Input.GetAxis("Horizontal");
+        float axisY = Input.GetAxis("Vertical");
+        Move(axisX,axisY);
+        if (Input.GetButtonDown("Fire1") && !is_firing) is_firing = true;
+        if (Input.GetButtonUp("Fire1") && is_firing)
+        {
+            is_firing = false;
+            firingCounter = fireCooldown;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (accelDown)
+            {
+                accelDown = false;
+            }
+            timebomb.Play();
+            Instantiate(sphere, gameObject.transform.position, gameObject.transform.rotation);
+            // 1*
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            transform.position = new Vector3(0.0f,0.0f,0.0f);
+            is_vertical = !is_vertical;
+            cb.switchCamPosRot(is_vertical);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (!accelDown)
+            {
+                tm.StartTimeDash();
+                accelDown = true;
+            }
+            else
+            {
+                tm.RestoreTimeDash();
+                accelDown = false;
+            }
+        }
+        if (is_firing)
+        {
+            Fire();
+            firingCounter -= Time.unscaledDeltaTime;
+        }
+        else is_firing = false;
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (ghostCounter < 3)
+            {
+                Instantiate(ghost, transform.position, transform.rotation);
+                ++ghostCounter;
+            }
+        }
+    }
+
+    public void Fire()
+    {
+        if (firingCounter <= 0.0f)
+        {
+            Transform t = gameObject.transform;
+            Instantiate(projectile, t.position, t.rotation);
+            firingCounter = fireCooldown;
+        }
+    }
+
+    public void Move(float Xinput, float YZinput) {
+        float nextPosX = ((Xinput * speedFactor) * (Time.deltaTime / Time.timeScale));
+        float nextPosYZ = ((YZinput * speedFactor) * (Time.deltaTime / Time.timeScale));
+        float coordAD, coordWS;
+        if (is_vertical) {
+            coordAD = gameObject.transform.position.x;
+            coordWS = gameObject.transform.position.z;
+        }
+        else {
+            coordAD = gameObject.transform.position.z;
+            coordWS = gameObject.transform.position.y;
+        }
+
+        if ((coordAD + nextPosX > -XLimit) && (coordAD + nextPosX < XLimit))
+            if (is_vertical) gameObject.transform.position += new Vector3(nextPosX, 0.0f, 0.0f);
+            else gameObject.transform.position += new Vector3( 0.0f, 0.0f, nextPosX);
+        if ((coordWS + nextPosYZ > -ZLimit) && (coordWS + nextPosYZ < ZLimit))
+            if (is_vertical) gameObject.transform.position += new Vector3(0.0f, 0.0f, nextPosYZ);
+            else gameObject.transform.position += new Vector3(0.0f, nextPosYZ, 0.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy" || other.tag == "EnemyProjectile")
+        {
+            if (!tm.slowDown)
+            {
+                Debug.Log("slowing");
+                slomo.Play();
+                tm.StartSloMo();
+            }
+            else
+            {
+                // Die or loose life
+            }
+        }
+    }
+}
