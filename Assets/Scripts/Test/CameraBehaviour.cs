288fd50bef53aa79be7143b840217d2f26828739
Max72590 <max-725@hotmail.com>
2018-07-08 18:17:15 +0200

    Updated player to switch between two views

--- Assets/Scripts/Test/CameraBehaviour.cs
+++ Assets/Scripts/Test/CameraBehaviour.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class CameraBehaviour : MonoBehaviour {
 
 
-    public Vector3 verticalPosition;
-    public Vector3 horizontalPosition;
-    public Vector3 verticalRotation;
-    public Vector3 horizontalRotation;
+    public Vector3 verticalPosition, horizontalPosition, verticalRotation, horizontalRotation;
     public float speed = 5.0f;
+    public float timeToMove = 1.0f;
+    private float t = 0.0f;
     public bool startRotating;
     public bool toVerticalPos;
+    private Vector3 startPos, targetPos;
+    private Quaternion startRot, targetRot;
+
 	// Use this for initialization
 	void Start () {
         startRotating = false;
@@ -41,11 +43,16 @@ public class CameraBehaviour : MonoBehaviour {
                 }
                 transform.rotation = Quaternion.Euler(0.0f, 0.0f,rotY);
             }*/
+            t += Time.deltaTime / timeToMove;
+            transform.position = Vector3.Lerp(startPos, targetPos, t);
+            transform.rotation = Quaternion.Euler( Vector3.Lerp(startRot.eulerAngles, targetRot.eulerAngles, t));
+            if ((Mathf.Abs(transform.position.x - targetPos.x) < 0.01) &&
+            (Mathf.Abs(transform.rotation.eulerAngles.x - targetRot.eulerAngles.x) < 0.01) ) startRotating = false;
         }
 	}
 
     public void switchCamPosRot(bool verticalOrientation) {
-         Quaternion q = Quaternion.identity;
+        /* Quaternion q = Quaternion.identity;
          if (verticalOrientation) {
              transform.position = verticalPosition;
              transform.rotation = Quaternion.Euler(verticalRotation.x, verticalRotation.y, verticalRotation.z) ;
@@ -54,9 +61,24 @@ public class CameraBehaviour : MonoBehaviour {
          {
              transform.position = horizontalPosition;
              transform.rotation = Quaternion.Euler(horizontalRotation.x, horizontalRotation.y, horizontalRotation.z);
-         }
-        //startRotating = true;
-        //toVerticalPos = verticalOrientation;
+         }*/
+        if (verticalOrientation)
+        {
+            targetPos = verticalPosition;
+            targetRot = Quaternion.Euler(verticalRotation.x, verticalRotation.y, verticalRotation.z);
+            startPos = horizontalPosition;
+            startRot = Quaternion.Euler(horizontalRotation.x, horizontalRotation.y, horizontalRotation.z);
+        }
+        else
+        {
+            targetPos = horizontalPosition;
+            targetRot = Quaternion.Euler(horizontalRotation.x, horizontalRotation.y, horizontalRotation.z);
+            startPos = verticalPosition;
+            startRot = Quaternion.Euler(verticalRotation.x, verticalRotation.y, verticalRotation.z);
+        }
+        t = 0;
+        startRotating = true;
+        toVerticalPos = verticalOrientation;
     }
 
 
--- Assets/Scripts/Test/SwitchablePlayerController.cs
+++ Assets/Scripts/Test/SwitchablePlayerController.cs
@@ -8,25 +8,22 @@ public class SwitchablePlayerController : MonoBehaviour {
     public bool is_vertical, is_firing;
     public float fireCooldown;
     public float speedFactor = 1.0f;
-    public GameObject projectile;
     public float XLimit = 10.0f;
     public float ZLimit = 5.0f;
     public float invul = 1.0f;
     public float sloMo = 2.0f;
-    private float firingCounter;
-    private bool  spaceDown, accelDown, firing ;
+    private float firingCounter, t;
+    private bool  accelDown, readjustPosition ;
     private TimeManager tm;
     private List<GameObject> ghostArray;
     public CameraBehaviour cb;
-    public GameObject sphere;
-    public GameObject ghost;
-    public AudioSource timebomb;
-    public AudioSource slomo;
+    public GameObject projectile, sphere, ghost;
+    public AudioSource timebomb, slomo;
+    public Vector3 readjustInitialPos;
     // Use this for initialization
     void Start() {
         firingCounter = 0.0f;
         is_firing = false;
-        spaceDown = false;
         accelDown = false;
         is_vertical = true;
         tm = GetComponent<TimeManager>();
@@ -38,45 +35,60 @@ public class SwitchablePlayerController : MonoBehaviour {
         float axisX = Input.GetAxis("Horizontal");
         float axisY = Input.GetAxis("Vertical");
         Move(axisX,axisY);
-        if (Input.GetButtonDown("Fire1") && !is_firing) {
+        ManageInput();
+        if (readjustPosition) ReadjustPlayer();
+        if (is_firing) {
+            Fire();
+            //firingCounter -= Time.unscaledDeltaTime;
+            firingCounter -= Time.deltaTime;
+        }        
+    }
+
+    public void ManageInput() {
+        if (Input.GetButtonDown("Fire1") && !is_firing)
+        {
             is_firing = true;
             foreach (GameObject g in ghostArray) g.GetComponent<TimeGhost>().StartFiring();
         }
-        if (Input.GetButtonUp("Fire1") && is_firing) {
+        if (Input.GetButtonUp("Fire1") && is_firing)
+        {
             is_firing = false;
             firingCounter = fireCooldown;
             foreach (GameObject g in ghostArray) g.GetComponent<TimeGhost>().StopFiring();
         }
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            if (accelDown) {
-                accelDown = false;
-            }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (accelDown) accelDown = false;            
             timebomb.Play();
             Instantiate(sphere, gameObject.transform.position, gameObject.transform.rotation);
         }
-        if (Input.GetKeyDown(KeyCode.V)) {
-            transform.position = new Vector3(0.0f,0.0f,0.0f);
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            readjustInitialPos = transform.position;
+            readjustPosition = true;
+            t = 0;
             is_vertical = !is_vertical;
             cb.switchCamPosRot(is_vertical);
         }
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            if (!accelDown) {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (!accelDown)
+            {
                 tm.StartTimeDash();
                 accelDown = true;
             }
-            else {
+            else
+            {
                 tm.RestoreTimeDash();
                 accelDown = false;
             }
         }
-        if (is_firing) {
-            Fire();
-            firingCounter -= Time.unscaledDeltaTime;
-        }
-        if (Input.GetKeyDown(KeyCode.G)) {
-            if (ghostArray.Count < 3) {
-                GameObject obj = Instantiate(ghost, transform.position, transform.rotation);                
-                if (ghostArray.Count > 0) obj.GetComponent<TimeGhost>().leader = ghostArray[(ghostArray.Count-1)].transform;
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (ghostArray.Count < 3)
+            {
+                GameObject obj = Instantiate(ghost, transform.position, transform.rotation);
+                if (ghostArray.Count > 0) obj.GetComponent<TimeGhost>().leader = ghostArray[(ghostArray.Count - 1)].transform;
                 else obj.GetComponent<TimeGhost>().leader = transform;
                 ghostArray.Add(obj);
             }
@@ -100,16 +112,31 @@ public class SwitchablePlayerController : MonoBehaviour {
             coordWS = gameObject.transform.position.z;
         }
         else {
-            coordAD = gameObject.transform.position.z;
+            coordAD = gameObject.transform.position.x;
             coordWS = gameObject.transform.position.y;
         }
-
-        if ((coordAD + nextPosX > -XLimit) && (coordAD + nextPosX < XLimit))
+        if ((coordAD + nextPosX > -XLimit) && (coordAD + nextPosX < XLimit)){
             if (is_vertical) gameObject.transform.position += new Vector3(nextPosX, 0.0f, 0.0f);
-            else gameObject.transform.position += new Vector3( 0.0f, 0.0f, nextPosX);
-        if ((coordWS + nextPosYZ > -ZLimit) && (coordWS + nextPosYZ < ZLimit))
+            else gameObject.transform.position += new Vector3(nextPosX, 0.0f, 0.0f);
+        }
+        if ((coordWS + nextPosYZ > -ZLimit) && (coordWS + nextPosYZ < ZLimit)){
             if (is_vertical) gameObject.transform.position += new Vector3(0.0f, 0.0f, nextPosYZ);
             else gameObject.transform.position += new Vector3(0.0f, nextPosYZ, 0.0f);
+        }
+        
+    }
+
+    public void ReadjustPlayer() {
+        if (is_vertical){
+            t += Time.deltaTime / 1.0f;
+            transform.position = Vector3.Lerp(readjustInitialPos, new Vector3(transform.position.x,0.0f, transform.position.z), t);
+            if ((Mathf.Abs(transform.position.y) < 0.01)) readjustPosition = false;
+        }
+        else {
+            t += Time.deltaTime / 1.0f;
+            transform.position = Vector3.Lerp(readjustInitialPos, new Vector3(transform.position.x, transform.position.y, 0.0f), t);
+            if ((Mathf.Abs(transform.position.z) < 0.01)) readjustPosition = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
