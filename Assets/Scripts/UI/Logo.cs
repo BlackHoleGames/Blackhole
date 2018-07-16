b319a96b9540c94751aace71ec2fee3225bddb73
Franky Pear <FrankPear85@hotmail.com>
2018-07-16 17:07:11 +0200

    Adding logoScreen and reorganize the sequence.

---
+++ Assets/Scripts/UI/Logo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Logo : MonoBehaviour {
+
+    public Image splashImage;
+    public string loadLevel;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+        FadeIn();
+        yield return new WaitForSeconds(3.0f);
+        FadeOut();
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+    }
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+    void FadeOut()
+    {
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
