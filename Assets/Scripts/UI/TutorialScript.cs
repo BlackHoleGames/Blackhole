75971cea9993f934404e8f0bc3e2b36bf6660269
Franky Pear <FrankPear85@hotmail.com>
2018-07-20 01:03:06 +0200

    Adding media for testingTutorial

--- Assets/Scripts/UI/TutorialScript.cs
+++ Assets/Scripts/UI/TutorialScript.cs
@@ -1,16 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 using System.Collections;
 
 public class TutorialScript : MonoBehaviour {
 
     public Text splashText;
     public string loadLevel;
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
     public bool PStart;
     // Use this for initialization
     IEnumerator Start()
     {
+        videoPlayer.Prepare();
+        WaitForSeconds ws = new WaitForSeconds(1.0f);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return ws;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audioSource.Play();
         splashText.canvasRenderer.SetAlpha(0.0f);
         PStart = false;
         for (int i = 0; i < 5; i++)
