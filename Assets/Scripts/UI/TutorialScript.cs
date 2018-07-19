cadfa6f9cab49ec491c32dfdb87c82e678fbe394
Franky Pear <FrankPear85@hotmail.com>
2018-07-19 19:48:49 +0200

    //Adding new sequences for scenes

---
+++ Assets/Scripts/UI/TutorialScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialScript : MonoBehaviour {
+
+    public Text splashText;
+    public string loadLevel;
+    public bool PStart;
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        splashText.canvasRenderer.SetAlpha(0.0f);
+        PStart = false;
+        for (int i = 0; i < 5; i++)
+        {
+            FadeInText();
+            yield return new WaitForSeconds(1.5f);
+            FadeOut();
+            yield return new WaitForSeconds(1.5f);
+        }
+        if (PStart)
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        //splashImage.canvasRenderer.SetAlpha(0.0f);
+        //splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+        //waitSeconds();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FadeInText()
+    {
+        splashText.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+    void FadeOut()
+    {
+        splashText.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
