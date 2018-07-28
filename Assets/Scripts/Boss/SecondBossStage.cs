12f30794f4093eb6f1d79f214e833c5e28e82de6
Max72590 <max-725@hotmail.com>
2018-07-28 19:43:05 +0200

    Added boss phase two scripts

--- Assets/Scripts/Boss/SecondBossStage.cs
+++ Assets/Scripts/Boss/SecondBossStage.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class SecondBossStage : MonoBehaviour {
 
+
+    public GameObject LeftWeakSpot, RightWeakSpot;
+    public GameObject[] Turrets;
+    public GameObject Eye;
+    public GameObject player;
+    private int WeakPointCounter;
+    private bool isVulnerable;
+    public Material Exposed;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.FindGameObjectWithTag("Player");
+        WeakPointCounter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        foreach (GameObject g in Turrets) {
+            g.transform.LookAt(player.transform.position);
+        }
 	}
+
+    public void WeakPointDone() {
+        Debug.Log("LIGMA");
+        ++WeakPointCounter;
+        if (WeakPointCounter >= 2)
+        {
+            Eye.GetComponent<Renderer>().material = Exposed;
+            isVulnerable = true;
+        }
+    }
 }
---
+++ Assets/Scripts/Boss/WeakPointScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakPointScript : MonoBehaviour {
+
+    private SecondBossStage sbs;
+    public Material matOn,matOff, hitMat;
+    private bool hit, materialHitOn;
+    public float life = 100.0f;
+	// Use this for initialization
+	void Start () {
+        sbs = transform.parent.GetComponent<SecondBossStage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (life > 0.0f)
+        {
+            if (materialHitOn)
+            {
+                gameObject.GetComponent<Renderer>().material = matOn;
+                materialHitOn = false;
+            }
+            if (hit)
+            {
+                hit = false;
+                gameObject.GetComponent<Renderer>().material = hitMat;
+                materialHitOn = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerProjectile")
+        {
+            hit = true;
+            if (life <= 0.0f)
+            {
+                sbs.WeakPointDone();
+                gameObject.GetComponent<Renderer>().material = matOff;
+                Destroy(this);
+            }
+            else {
+                life -= other.GetComponent<Projectile>().damage;
+            }
+        }
+    }
+}
