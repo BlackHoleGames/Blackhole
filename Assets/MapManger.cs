29c5246e1d3bc63ca7809493fb7805ef3d3a672b
Max72590 <max-725@hotmail.com>
2018-08-04 20:30:18 +0200

    added map manager

---
+++ Assets/MapManger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManger : MonoBehaviour {
+
+    public enum Stages {INTRO, METEORS_TIMEWARP, METEORS_ENEMIES, MINIBOSS, STRUCT_TIMEWARP,
+        STRUCT_ENEMIES, BOSS, ESCAPE}
+
+    public Stages actualStage = Stages.INTRO ;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (actualStage)
+        {
+            case Stages.INTRO:
+                break;
+            case Stages.METEORS_TIMEWARP:
+                break;
+            case Stages.METEORS_ENEMIES:
+                break;
+            case Stages.MINIBOSS:
+                break;
+            case Stages.STRUCT_TIMEWARP:
+                break;
+            case Stages.STRUCT_ENEMIES:
+                break;
+            case Stages.BOSS:
+                break;
+            case Stages.ESCAPE:
+                break;
+        }
+    }
+
+    public void GoToNextStage() {
+        ++actualStage;
+    }
+
+}
