faaca7b56ddb953ae6890c30f67380b89fd030d0
Max72590 <max-725@hotmail.com>
2018-08-13 20:25:39 +0200

    Added enemy destroy script

---
+++ Assets/Scripts/Enemies/EnemyDestroyed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDestroyed : MonoBehaviour {
+
+    public float timeBeforeDestroy = 1.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeBeforeDestroy -= Time.deltaTime;
+        if (timeBeforeDestroy < 0.0f) CallDestroy();
+	}
+
+    public void CallDestroy() {
+        foreach (Transform child in transform)
+        {
+            Instantiate(Resources.Load("Explosion"), child.position, child.rotation);
+            Destroy(child.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
