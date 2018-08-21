317af6d55c688fbbfc330e213295cfd5b52ee679
Franky Pear <FrankPear85@hotmail.com>
2018-08-21 19:55:52 +0200

    //Adding life Sniper, alertMode

---
+++ Assets/PlayerAlert.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAlert : MonoBehaviour {
+    public GameObject psAlert;
+    private IEnumerator DeathTimerSequence;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.FindGameObjectWithTag("Player").GetComponent<SwitchablePlayerController>().isAlert
+            && !GameObject.FindGameObjectWithTag("Player").GetComponent<SwitchablePlayerController>().isDeath)
+        {
+            DeathTimerSequence = DeathSequence(3.0f);
+            StartCoroutine(DeathTimerSequence);
+        }
+	}
+    IEnumerator DeathSequence(float waitToDeath)
+    {
+        //Destroy();
+        psAlert.SetActive(true);
+        yield return new WaitForSeconds(waitToDeath);
+        psAlert.SetActive(false);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<SwitchablePlayerController>().isAlert = false;
+        //Restore();
+    }
+}
--- Assets/Scripts/UI/ScoreScript.cs
+++ Assets/Scripts/UI/ScoreScript.cs
@@ -16,27 +16,27 @@ public class ScoreScript : MonoBehaviour {
 	void Update () {
         //score = score + 10;
         ScoreTextValue.text = score.ToString();
-        if (ScoreScript.multiplierScore == 1.0f)
-        {
-            GameObject.FindGameObjectWithTag("MultiplierX1").GetComponent<Image>().fillAmount +=
-                    MultiplierIncrease * Time.unscaledDeltaTime;
-            GameObject.FindGameObjectWithTag("MultiplierX2").GetComponent<Image>().fillAmount = 0.0f;
-            GameObject.FindGameObjectWithTag("MultiplierX3").GetComponent<Image>().fillAmount = 0.0f;
-        
-        }
-        else if (ScoreScript.multiplierScore == 2.0f)
-        {
-            GameObject.FindGameObjectWithTag("MultiplierX1").GetComponent<Image>().fillAmount = 0.0f;
-            GameObject.FindGameObjectWithTag("MultiplierX2").GetComponent<Image>().fillAmount +=
-                MultiplierIncrease * Time.unscaledDeltaTime;
-            GameObject.FindGameObjectWithTag("MultiplierX3").GetComponent<Image>().fillAmount = 0.0f;
-        }
-        else if (ScoreScript.multiplierScore == 2.0f)
-        {
-            GameObject.FindGameObjectWithTag("MultiplierX1").GetComponent<Image>().fillAmount = 0.0f;
-            GameObject.FindGameObjectWithTag("MultiplierX2").GetComponent<Image>().fillAmount = 0.0f;
-            GameObject.FindGameObjectWithTag("MultiplierX3").GetComponent<Image>().fillAmount +=
-                MultiplierIncrease * Time.unscaledDeltaTime;
-        }
+//        if (ScoreScript.multiplierScore == 1.0f)
+//        {
+//            GameObject.FindGameObjectWithTag("MultiplierX1").GetComponent<Image>().fillAmount +=
+//                    MultiplierIncrease * Time.unscaledDeltaTime;
+//            GameObject.FindGameObjectWithTag("MultiplierX2").GetComponent<Image>().fillAmount = 0.0f;
+//            GameObject.FindGameObjectWithTag("MultiplierX3").GetComponent<Image>().fillAmount = 0.0f;
+//        
+//        }
+//        else if (ScoreScript.multiplierScore == 2.0f)
+//        {
+//            GameObject.FindGameObjectWithTag("MultiplierX1").GetComponent<Image>().fillAmount = 0.0f;
+//            GameObject.FindGameObjectWithTag("MultiplierX2").GetComponent<Image>().fillAmount +=
+//                MultiplierIncrease * Time.unscaledDeltaTime;
+//            GameObject.FindGameObjectWithTag("MultiplierX3").GetComponent<Image>().fillAmount = 0.0f;
+//        }
+//        else if (ScoreScript.multiplierScore == 2.0f)
+//        {
+//            GameObject.FindGameObjectWithTag("MultiplierX1").GetComponent<Image>().fillAmount = 0.0f;
+//            GameObject.FindGameObjectWithTag("MultiplierX2").GetComponent<Image>().fillAmount = 0.0f;
+//            GameObject.FindGameObjectWithTag("MultiplierX3").GetComponent<Image>().fillAmount +=
+//                MultiplierIncrease * Time.unscaledDeltaTime;
+//        }
     }
 }
