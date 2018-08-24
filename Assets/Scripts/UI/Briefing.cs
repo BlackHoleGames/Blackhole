e55b1d008f6e6f66e176e03881a40f2734114957
Franky Pear <FrankPear85@hotmail.com>
2018-08-24 16:16:36 +0200

    //Updating all UI Scenes, adding curtains and new sequences

--- Assets/Scripts/UI/Briefing.cs
+++ Assets/Scripts/UI/Briefing.cs
@@ -5,26 +5,25 @@ using System.Collections;
 using System;
 
 public class Briefing : MonoBehaviour {
-    public Text splashText;
-    public bool PStart;
+    public Text briefingText;
+    public Image curtainImage;
     // Use this for initialization
     IEnumerator Start () {
-        splashText.canvasRenderer.SetAlpha(0.0f);
+        briefingText.canvasRenderer.SetAlpha(0.0f);
+        curtainImage.canvasRenderer.SetAlpha(0.0f);
         FadeIn();
         yield return new WaitForSeconds(3.0f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
     void FadeIn()
     {
-        splashText.CrossFadeAlpha(1.0f, 1.5f, false);
+        briefingText.CrossFadeAlpha(1.0f, 1.5f, false);
     }
-
-    // Update is called once per frame
-    void Update () {
-        //if (Input.GetKeyDown("enter"))
-        //{
-        //    PStart = true;
-        //}
+    void FadeOut()
+    {
+        briefingText.CrossFadeAlpha(0.0f, 1.5f, false);
+        curtainImage.CrossFadeAlpha(1.0f, 2.5f, false);
     }
 }
