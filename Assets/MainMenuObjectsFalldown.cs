c9b55d0cf27eb17e843f9d982e5d4045ee107878
Esteban Marín Illana <esteban81g@gmail.com>
2018-09-15 13:06:56 +0200

    tittle screen cam 2 and script created

---
+++ Assets/MainMenuObjectsFalldown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuObjectsFalldown : MonoBehaviour {
+
+	 
+	public Vector3 axis;
+	public float speed = 0.0f;
+	public float fallbackDistance = 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other){
+
+	}
+}
