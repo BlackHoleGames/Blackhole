826db780d4015dae377db79a112bcf7094df8f2a
Franky Pear <FrankPear85@hotmail.com>
2018-09-26 02:31:15 +0200

    //TextBlackhole before impact and adding some ui properties

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -10,18 +10,24 @@ public class ChangeScene : MonoBehaviour {
     private IEnumerator SceneCloseTimerSequence;
     private IEnumerator SceneOpenTimerSequence;
     private IEnumerator SceneBHSequence;
+    private IEnumerator TextBHSequence;
     public Image BlackFade;
+    public Text BHText;
     public bool shutdown = false;
+    public bool isBlackHoleTime = false;
     private bool activateDeath =false;
     private bool activateBH = false;
+    private bool activateTextBH = false;
     public float SecondsInBH = 16.0f;
     public float SecondsOutBH = 3.0f;
     public float SecondsToWaitDestroy = 2.0f;
     public float SecondsToWaitEnd = 2.0f;
+    public float SecondsTextFlick = 0.5f;
     void Start()
     {
         Cursor.visible = false;
         BlackFade.canvasRenderer.SetAlpha(1.0f);
+        BHText.canvasRenderer.SetAlpha(0.0f);
         SceneOpenTimerSequence = FadeToInitLevel(2.0f);
         StartCoroutine(SceneOpenTimerSequence);
         changeScene = false;
@@ -35,22 +41,19 @@ public class ChangeScene : MonoBehaviour {
             SceneCloseTimerSequence = FadeToEndingLevel(SecondsToWaitEnd);
             StartCoroutine(SceneCloseTimerSequence);
         }
-        //else if (GameObject.FindGameObjectWithTag("Player").GetComponent<BlackHoleTransition>()!=null
-        //    && GameObject.FindGameObjectWithTag("Player").GetComponent<BlackHoleTransition>().startCount
-        //    && !activateBH)
-        //{
-        //    SceneBHSequence = FadeToInitBH();
-        //    StartCoroutine(SceneBHSequence);
-        //}
+        if (isBlackHoleTime && !activateTextBH)
+        {
+            activateTextBH = true;
+            TextBHSequence = FadeToTextBH(SecondsTextFlick);
+            StartCoroutine(TextBHSequence);
+
+        }
+
     }
-    //public void Shutdown()
-    //{
-    //    changeScene = true;
-    //    SceneCloseTimerSequence = FadeToEndingLevel(3.0f);
-    //    StartCoroutine(SceneCloseTimerSequence);
-    //}
+
 
     public void StartBlackHoleSequence() {
+        isBlackHoleTime = true;
         SceneBHSequence = FadeToInitBH();
         StartCoroutine(SceneBHSequence);
     }
@@ -78,12 +81,26 @@ public class ChangeScene : MonoBehaviour {
     }
     IEnumerator FadeToInitBH()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(5.0f);
         RumblePad.RumbleState = 7;
+        isBlackHoleTime = false;
         BlackFade.CrossFadeAlpha(1.0f, 3.0f, false);
         yield return new WaitForSeconds(SecondsInBH);
         BlackFade.CrossFadeAlpha(0.0f, 5.0f, false);
         yield return new WaitForSeconds(SecondsOutBH);
     }
-
+    IEnumerator FadeToTextBH(float flickText)
+    {
+        BHText.CrossFadeAlpha(1.0f, flickText, false);
+        
+        //var tempColor = BHText.color;
+        //tempColor.a = 1f;
+        //BHText.color = tempColor;
+        yield return new WaitForSeconds(flickText);
+        BHText.CrossFadeAlpha(0.0f, flickText, false);
+        //tempColor.a = 0.0f;
+        //BHText.color = tempColor;
+        yield return new WaitForSeconds(flickText);
+        activateTextBH = true;
+    }
 }
