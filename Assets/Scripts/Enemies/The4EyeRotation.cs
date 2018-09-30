a6133424bc4fd80713c5b297893002ccd37617b7
Max72590 <max-725@hotmail.com>
2018-09-30 17:15:45 +0200

    updated boss eye rotation

--- Assets/Scripts/Enemies/The4EyeRotation.cs
+++ Assets/Scripts/Enemies/The4EyeRotation.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class The4EyeRotation : MonoBehaviour {
     public float speed = 1.0f;
+    private TimeBehaviour tb;
     // Use this for initialization
     void Start () {
-		
+        tb = GetComponent<TimeBehaviour>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0.0f, 0.0f, -speed * Time.deltaTime);
+        transform.Rotate(0.0f, 0.0f, -speed * Time.deltaTime*tb.scaleOfTime);
     }
 }
