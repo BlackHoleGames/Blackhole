8ec0d88ade5fbc4b18f466d8f72bcbfa6bd3e1eb
Max72590 <max-725@hotmail.com>
2018-10-02 23:18:53 +0200

    added tutorial scripts

---
+++ Assets/Scripts/CallTutorial1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallTutorial1 : MonoBehaviour {
+
+    public float counter = 4.0f;
+    private SwitchablePlayerController spc;
+	// Use this for initialization
+	void Start () {
+        spc = GameObject.FindGameObjectWithTag("Player").GetComponent<SwitchablePlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        counter -= Time.deltaTime;
+        if (counter <= 0.0f) {
+            spc.StopTimeTutorial1();
+        }
+	}
+}
