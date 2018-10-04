9c62f1d62f0b85ef1644eb7ff44d3b634d277e30
Franky Pear <FrankPear85@hotmail.com>
2018-10-05 01:39:40 +0200

    //Ending increase 1.5f

--- Assets/Scripts/UI/EndingScript.cs
+++ Assets/Scripts/UI/EndingScript.cs
@@ -125,7 +125,7 @@ public class EndingScript : MonoBehaviour {
     IEnumerator ShutdownGame()
     {
         Curtain.CrossFadeAlpha(1.0f, 3.0f, false);
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(4.5f);
         SceneManager.LoadScene(6);
     }
 }
